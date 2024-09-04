0f9c3d8e76292adae59114aaf21e55989c89a679
mr-steal-ur-code <cjkarkut@gmail.com>
2024-09-04 07:28:13 -0500

    use screensize hook with slider settings

--- src/components/Book Carousel/BookCarousel.tsx
+++ src/components/Book Carousel/BookCarousel.tsx
@@ -2,47 +2,44 @@ import Slider, { Settings } from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./bookcarousel.css";
+import { useMediaQuery } from "../../hooks/useMediaQuery";
 
 type CarouselProps = {
 	objArray: any[];
 };
 
 const Carousel: React.FC<CarouselProps> = ({ objArray }) => {
+	const matches = useMediaQuery("(max-width:28rem)");
 	const settings: Settings = {
 		dots: true,
 		infinite: true,
 		speed: 500,
-		slidesToShow: 2,
+		slidesToShow: matches ? 1 : 2,
 		slidesToScroll: 1,
 		adaptiveHeight: true,
-		arrows: objArray?.length >= 2,
+		arrows: !matches && objArray?.length >= 2,
 	};
-	console.log(objArray?.length);
 
 	const handleBookClick = (id: string) => {
 		console.log(id);
 	};
 
 	return (
-		<div>
-			<Slider {...settings}>
-				{objArray?.map?.((obj: CookBook) => (
-					<div
-						key={obj.id}
-						className="cursor-pointer book-container"
-						onClick={() => handleBookClick(obj.id!)}
-					>
-						<div className="book-spine" />
-						<div className="book-pages" />
-						<div className="book-content">
-							<p className="text-sm bg-orange-50 rounded-md p-1">
-								{obj?.title}
-							</p>
-						</div>
+		<Slider {...settings}>
+			{objArray?.map?.((obj: CookBook) => (
+				<div
+					key={obj.id}
+					className="cursor-pointer book-container"
+					onClick={() => handleBookClick(obj.id!)}
+				>
+					<div className="book-spine" />
+					<div className="book-pages" />
+					<div className="book-content">
+						<p className="text-sm bg-orange-50 rounded-md p-1">{obj?.title}</p>
 					</div>
-				))}
-			</Slider>
-		</div>
+				</div>
+			))}
+		</Slider>
 	);
 };
 
